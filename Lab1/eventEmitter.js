81dfa1a12e0fdc5315a6382125242fa70fccebc2
ayush kumar pandey <ayushkumarpandey123456789@gmail.com>
2026-07-17 15:42:39 +0530

    introductiontojs

---
+++ Lab1/eventEmitter.js
@@ -0,0 +1,25 @@
+    import {EventEmitter} from "node:events";
+    const task = new EventEmitter();
+    
+    const sayHi = (name) => {
+        console.log(`Logged in ${name}`);
+
+    };
+
+    task.on('greet', sayHi)
+    task.on('greet', () => {
+        console.log("Logged out");
+    });
+    task.once('greet', () => {
+        console.log("System started.....");
+    });
+    const shuttingDown = (name) => {
+        console.log(`System is shutting down by ${name}`);
+    }
+    
+
+    task.emit('greet', 'Ayush');
+    task.emit('greet', 'Piyush');
+    task.emit('greet', 'Braj');
+    task.emit('exit', 'Manager');
+    task.off('greet', shuttingDown);
---
+++ Lab1/index.js
@@ -0,0 +1,21 @@
+//write a function who take your name and and print it with hello message
+function sayHello(name) {
+    console.log(`Hello, ${name}`);
+}
+
+const sayHi = (name) => {
+    console.log(`Hi, ${name}`);
+}
+
+const sumTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+const sum = (a,b) => a + b;
+
+sayHello("Ayush");
+sayHi("Piyush");
+console.log("sum=",sumTwoNumbers(5, 10));
+console.log("sum=",sum(5, 10));
+
+
