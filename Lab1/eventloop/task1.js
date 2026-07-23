cd355c105f9c85d850286d316a771e7656cdd07a
ayush kumar pandey <ayushkumarpandey123456789@gmail.com>
2026-07-23 16:26:04 +0530

    eventlooptask2

--- Lab1/eventloop/task1.js
+++ Lab1/eventloop/task1.js
@@ -1,18 +1,27 @@
 //make three functions with name f1,f2 and f3
-const f1 = () =>{
-    console.log("f1");
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
 };
-const f2 = () =>{
-    console.log("f2");
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
 };
-const f3 = () =>{
-    console.log("f3");
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
 };
-function main(){
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log(" end main");
+function main() {
+  console.log("main");
+  f1();
+  console.log("end main");
 }
 main();
+//synchronous call: java script is single threaded and synchronous programming language
+//in asynchronous we use eventloop to manage the call stack
+//the asynchrous call using timers: 1. set time out 2. set immediate 3. process.next tick 4. set interval
