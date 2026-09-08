f205d3b2070ac242c939db1ae390cafd8f094d54
ayush kumar pandey <ayushkumarpandey123456789@gmail.com>
2026-09-08 10:06:19 +0530

    add helper method

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,10 +1,36 @@
 import http from "http";
-const server = http.createServer(); 
+//import * as teams from 'teams.js';
+import { getAllTeams } from './teams.js';
+
+const PORT = 5000;
+const sendJson = (res, statusCode, data) => {
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+    res.end(data === "undefined" ? "" : JSON.stringify(data));
+};
+const parseJSONBody = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            try {
+                resolve(JSON.parse(body));
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
+const server = http.createServer((req, res) => {});
+
+
+
 server.on("request", (req, res) => {
    
     res.end("<h1> SIH Internal<h1>");
 });
 
-server.listen(5000,() =>{
-    console.log("Server is running");
+server.listen(PORT,() =>{
+    console.log(`Server is running on port ${PORT}`);
 });
